15df7117f1682cdf89b7fc360c1b12477946d169
Bruno Yin <bruno.yin@gmail.com>
2019-03-14 20:53:18 -0400

    Corrected Error handling

--- docker/dotnet/build.ps1
+++ docker/dotnet/build.ps1
@@ -9,15 +9,33 @@ docker build `
 --build-arg GID=$(id -g) `
 -t yin/dotnet:2.2 .
 
-$w.Stop()
 if ($?){
+    $w.Stop()
     "yin/dotnet:2.2 has been built successfully in {0:#,###.##0} seconds" -f $w.Elapsed.TotalSeconds
 }else {
+    $w.Stop()
     "Failed to build yin/dotnet:2.2, time used: {0:#,###.##0} seconds" -f $w.Elapsed.TotalSeconds
 }
 
+# to test, 
+# 1. Make sure postgres docker server is running, set up properly
+# $env:PGHOST = '192.168.0.48'
+# $env:PGDATABASE = 'bruno'
+# $env:PGUSER = 'bruno'
+# $env:PGPASSWORD = 'bruno'
+# 2. cd $TEST_DRIVE_ROOT/dotnet/CmdlineApp
+# 
+# docker run -it --rm `
+# -e PGHOST=$env:PGHOST  `
+# -e PGDATABASE=$env:PGDATABASE `
+# -e PGUSER=$env:PGUSER `
+# -e PGPASSWORD=$env:PGPASSWORD `
+# -v $PWD:/app `
+# --workdir /app `
+# yin/dotnet:2.2 dotnet run
+
 <#
-Wehn behind corporate firewall
+When behind corporate firewall
 
 docker build `
 --build-arg UID=$(id -u) `
