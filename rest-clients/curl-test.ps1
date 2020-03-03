b1aa7cb234bca7e85f2538233128f75c4538f9de
Bruno Yin <bruno.yin@gmail.com>
2020-03-02 22:55:44 -0500

    fixed curl doc

--- rest-clients/curl-test.ps1
+++ rest-clients/curl-test.ps1
@@ -13,3 +13,22 @@ $w
 "`n" + ('#*^' * 40) + "`n"
 "`n`n{2:#,###} calls done in {0:#,###.##0} seconds, average {1:#.##0} calls per second`n" -f $w.Elapsed.TotalSeconds, $($total / $w.Elapsed.TotalSeconds),$total
 "=" * 90
+
+<#
+
+# obviously this is slower because we have to create a process every time. When using programming language, we create a process once only
+
+.\curl-test.ps1 -run_url http://192.168.1.250:9876/run -t 100
+
+IsRunning Elapsed          ElapsedMilliseconds ElapsedTicks
+--------- -------          ------------------- ------------
+    False 00:00:03.9927192                3992     39927192
+
+#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^#*^
+
+
+
+100 calls done in 3.993 seconds, average 25.046 calls per second
+
+==========================================================================================
+#>
